7d53e07e3855e04d0017f3528bd112ee1471d51f
Jamin <765426599@qq.com>
2025-06-27 15:54:49 +0800

    edit the config of experiment

--- visualization/app/alogrithms/config.js
+++ visualization/app/alogrithms/config.js
@@ -1,21 +1,21 @@
 export const coreNums = 4
 
-export const Turn = 50
+export const Turn = 1e8
 
 export const updateExperimentFrequency = 1
-export const InitialCoreState = [
-    ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
-    ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
-    ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
-    ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
-    ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
-]
-
-
 // export const InitialCoreState = [
-//     ['Idel', 'Idel', 'Idel', 'Idel'],
+//     ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
+//     ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
+//     ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
+//     ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
+//     ['Broke', 'Idel', 'Idel', 'Idel', 'Idel'],
 // ]
 
-// export const ClusterNumber = 1;
 
-export const ClusterNumber = 5;
+export const InitialCoreState = [
+    ['Idel', 'Idel', 'Idel', 'Idel'],
+]
+
+export const ClusterNumber = 1;
+
+// export const ClusterNumber = 5;
